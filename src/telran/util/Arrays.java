5687911f4db367fbe3e11593de75d85350d3f799
Yuragranovsky <yuragranovsky@gmail.com>
2024-03-28 11:05:31 +0200

    HW #10 generic bubbleSort / EvenOddComparator

--- src/telran/util/Arrays.java
+++ src/telran/util/Arrays.java
@@ -29,6 +29,24 @@ public class Arrays {
 		
 	}
 	public static <T> void bubbleSort(T[] array, Comparator<T> comp) {
-		//TODO
+		boolean isUnsorted = true;
+		int length = array.length;
+		while(isUnsorted) {
+			length--;
+			isUnsorted = false;
+			for(int i = 0; i < length; i++) {
+				if(comp.compare(array[i], array[i + 1]) > 0) {
+					swap(array, i, i + 1);
+					isUnsorted = true;
+				}
+			}
+		}
+	}
+
+	private static <T> void swap(T[] array, int i, int j) {
+		T tmp = array[i];
+		array[i] = array[j];
+		array[j] =tmp;
+		
 	}
 }
--- src/telran/util/test/EvenOddComparator.java
+++ src/telran/util/test/EvenOddComparator.java
@@ -5,11 +5,21 @@ import java.util.Comparator;
 public class EvenOddComparator implements Comparator<Integer> {
 
 	@Override
-	public int compare(Integer o1, Integer o2) {
+	public int compare(Integer num1, Integer num2) {
 		// TODO 
 		//first even numbers sorted in the ascending order
 		//last odd numbers sorted in the descending order
-		return 0;
+		boolean isNum1Even = num1 % 2 == 0;
+		boolean isNum2Even = num2 % 2 == 0;
+		int res = 1;
+		if (isNum1Even && isNum2Even) {
+			res = num1 - num2;
+		} else if (!isNum1Even && !isNum2Even) {
+			res = num2 - num1;
+		} else if (isNum1Even) {
+			res = -1;
+		}
+		return res;
 	}
 
 }
