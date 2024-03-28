60a29d268f1f524f9232a5a0d953fe8eb1428fc7
Yuragranovsky <yuragranovsky@gmail.com>
2024-03-28 19:25:10 +0200

    HW #10 removed TODO comment

--- src/telran/util/test/EvenOddComparator.java
+++ src/telran/util/test/EvenOddComparator.java
@@ -6,16 +6,15 @@ public class EvenOddComparator implements Comparator<Integer> {
 
 	@Override
 	public int compare(Integer num1, Integer num2) {
-		// TODO 
 		//first even numbers sorted in the ascending order
 		//last odd numbers sorted in the descending order
 		boolean isNum1Even = num1 % 2 == 0;
 		boolean isNum2Even = num2 % 2 == 0;
 		int res = 1;
 		if (isNum1Even && isNum2Even) {
-			res = num1 - num2;
+			res = Integer.compare(num1, num2);
 		} else if (!isNum1Even && !isNum2Even) {
-			res = num2 - num1;
+			res = Integer.compare(num2, num1);
 		} else if (isNum1Even) {
 			res = -1;
 		}
